f4ff47c117572d63b0b5ee8ed52aec8aa0acfe4c
Noldor5150 <ruikis.paulius@gmail.com>
2020-04-22 19:55:48 +0300

    Add project files.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var studentList = new List<Student>()
+            {
+
+                new Student(){Id = 0, Name = "Algis", Age = 17 , AverageMark = 7, IsGettingTuition = false},
+               new Student(){Id = 1, Name = "Kestas", Age = 27 , AverageMark = 8, IsGettingTuition = true},
+               new Student(){Id = 2, Name = "Pranas", Age = 22 , AverageMark = 7, IsGettingTuition = true},
+               new Student(){Id = 3, Name = "Anupras", Age = 18 , AverageMark = 6, IsGettingTuition = false},
+               new Student(){Id = 4, Name = "Zigmas", Age = 19 , AverageMark = 9, IsGettingTuition = true},
+               new Student(){Id = 5, Name = "Emilis", Age = 22 , AverageMark = 10, IsGettingTuition = false},
+            };
+/*
+            var result = studentList.FindAll(student => student.Age > 20);
+            result.ForEach(student => Console.WriteLine(student.Name));
+
+            var result2 = studentList.FindAll(student => student.AverageMark > 8 && student.IsGettingTuition);
+            result2.ForEach(student => Console.WriteLine(student.Name));*/
+
+            var result3 = studentList.FindAll(student => student.AverageMark > 4 && student.Name.Length < 7);
+            result3.ForEach(student => Console.WriteLine(student.Name));
+        }
+     
+    }
+}
---
+++ ConsoleApp12/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp12
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double AverageMark { get; set; }
+        public bool IsGettingTuition { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"Student {Id} {Name} - age:{Age} mark:{AverageMark} tuition:{IsGettingTuition}";
+        }
+    }
+
+}
